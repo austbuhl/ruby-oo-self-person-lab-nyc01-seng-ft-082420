92447f88d8a0b719a280122651d968dba524b84d
AB <austinbuhler@gmail.com>
2020-08-26 11:56:22 -0400

    Second Commit

--- lib/person.rb
+++ lib/person.rb
@@ -2,54 +2,62 @@
 
 class Person
 
-    attr_reader :name
-    attr_accessor :bank_account, :happiness, :hygiene, :friend
+    attr_reader :name 
+    attr_accessor :bank_account, :friend, :hygiene, :happiness
     
     def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
         @name = name
         @bank_account = bank_account
         @happiness = happiness
-        @hygiene = hygiene    
+        @hygiene = hygiene
     end
 
-    def clean?(hygiene)
-        @hygiene = hygiene
+    # def hygiene=(hygiene)
+    #     @hygiene = hygiene
         
-        if hygiene > 7
-            else return false
+    #     if hygiene < 0
+    #         hygiene = 0
+    #     elsif hygiene > 10
+    #         hygiene = 10
+    #     else
+    #         hygiene
+    #     end
+    # end
+
+    def clean?
+        
+        if self.hygiene > 7 
+            return true
+        else 
+            return false
         end
     end
 
-    def happy?(happiness)
-        @happiness = happiness
+    def happy?
         
-        if happiness > 7
-            else return false
+        if self.happiness > 7 
+            return true
+        else 
+            return false
         end
     end
 
     def get_paid(salary)
 
         @bank_account += salary
-        return "All about the benjamins."
+        return "all about the benjamins"
 
     end
 
-    def take_bath(hygiene)
-
-        @hygiene=hygiene
-        hygiene += 4
-        return "Rub-a-dub just relaxing in the tub."
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
-    def work_out(happiness, hygiene)
-
-        @happiness=happiness
-        @hygiene=hygiene
-
-        happiness += 2
-        hygiene -= 3
-        return "Another one bites the dust."
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
 
     end
 
@@ -59,9 +67,8 @@ class Person
         self.happiness += 3
         friend.happiness += 3
 
-        if self == "Stella" && friend == "Felix"
-            return "Hi Felix! It's Stella. How are you?"
-        end
+            return "Hi #{friend.name}! It's #{self.name}. How are you?"
+
     end
 
     def start_conversation(friend, topic)
@@ -70,9 +77,11 @@ class Person
         if topic == "politics"
             self.happiness -= 2
             friend.happiness -= 2
+            return "blah blah partisan blah lobbyist"
         elsif topic == "weather"
             self.happiness += 3
             friend.happiness += 3
+            return "blah blah sun blah rain"
         else return "blah blah blah blah blah"
         end
 
@@ -83,4 +92,4 @@ end
 # ACTION ITEMS
 # min/max for happiness & hygiene
 # cleanup clean? and happy? methods
-# test the call_friend and start_conversation methods
+# test the call_friend and start_conversation methods䈀
